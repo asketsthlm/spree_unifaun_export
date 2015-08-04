11ce724ab5904baf98d5a5979e947c3bcca9027a
Mehdi Rejraji <mehdi.rejraji@gmail.com>
2015-08-04 17:39:03 +0200

    shp status

--- app/overrides/admin_orders_index_shipment_status.rb
+++ app/overrides/admin_orders_index_shipment_status.rb
@@ -1,16 +1,15 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/index',
   name: 'status_order_index_thead',
-  insert_before: 'erb[loud]:contains("I18n.t(:email, :scope => \'activerecord.attributes.spree/order\')")',
+  insert_after: 'erb[loud]:contains("I18n.t(:total, :scope => \'activerecord.attributes.spree/order\')")',
   text: '
   <th>Shp Status</th>
 '
 )
-
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/index',
   name: 'status_order_index_tbody',
-  insert_before: 'erb[silent]:contains("if order.user")',
+  insert_before: 'td.actions.actions-1',
   text: '
   <td><%= order.shipments ? order.shipments.first.tracking_status : "" %></td>
 '
