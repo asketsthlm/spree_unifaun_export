59f24368e60205bfeebf67ed2e001e6ef425391e
Mehdi Rejraji <mehdi.rejraji@gmail.com>
2015-08-04 16:52:20 +0200

    tracking status in backend view

--- app/overrides/admin_orders_show_total_weight.rb
+++ app/overrides/admin_orders_show_total_weight.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :virtual_path => 'spree/admin/orders/_shipment',
   :name => 'display_total_weight',
   :insert_after => "span.shipment-number",
-  :text => "&nbsp;<span class='shipment-weight'>(<%= shipment.total_weight %>g)</span>"
+  :text => "&nbsp;<span class='shipment-status'>(<%= shipment.tracking_status %>)</span>&nbsp;<span class='shipment-weight'>(<%= shipment.total_weight %>g)</span>"
 )
